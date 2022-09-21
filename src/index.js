cbfc470ad6ad2fdff6ac7a2cae3bb9986ad5715e
Diego Candido <diegocan.sou@gmail.com>
2022-09-20 22:19:16 -0300

    Versão Zerada

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import ReactDOM  from "react-dom";
+
+function App(){
+    return(
+        <div>
+            Oiii
+        </div>
+    )
+}
+
+ReactDOM.render(<App/>, document.querySelector('.root'))
