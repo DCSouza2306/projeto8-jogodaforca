0ad245274b08172bceccc6a27d85714e7663923d
Diego Candido <diegocan.sou@gmail.com>
2022-09-21 23:21:11 -0300

    Versão com as letras sendo selecionadas e exibidas

--- src/App.js
+++ src/App.js
@@ -1,20 +1,22 @@
-import palavras from "./palavras"
 import forca0 from "./img/forca0.png"
 import {useState} from "react"
+import palavras from "./palavras"
 
 
 function App() {
-    const [letraHabilitada, setLetraHabilitada] = useState(false)
+    const [jogoHabilitado, setJogoHabilitado] = useState(false);
+    const [palavraEscolhida, setPalavraEscolhida] = useState([]);
+    const [letraEscolhida, setLetraEscolhida] = useState([]);
     
-    function imprimirLetras(a){
+    function imprimirLetras(a,index){
         return(
-            <span className={(letraHabilitada)?"letra habilitado":"letra"} onClick={selecionarLetra}>{a}</span>
+            <span key={index} className={(jogoHabilitado)?"letra habilitado":"letra"} onClick={()=>selecionarLetra(a)}>{a}</span>
         )
     }
 
-    function selecionarLetra(){
-        if(letraHabilitada){
-            alert("O jogo iniciou")
+    function selecionarLetra(a){
+        if(jogoHabilitado){
+            setLetraEscolhida([...letraEscolhida, a]);
         } else{
             alert("o jogo ainda não iniciou")
         }
@@ -22,8 +24,22 @@ function App() {
     }
 
     function habilitarJogo(){
-        const jogoHabilitado = true;
-        setLetraHabilitada(jogoHabilitado);
+        const iniciaJogo = true;
+        setJogoHabilitado(iniciaJogo);
+        const palavraSelecionada = palavras[Math.floor(Math.random()*palavras.length)]
+        const arrayPalavra = [];
+
+        for(let i=0; i<palavraSelecionada.length;i++){
+            arrayPalavra.push(palavraSelecionada[i]);
+        }
+        setPalavraEscolhida([...arrayPalavra])
+        console.log(arrayPalavra);
+    }
+
+    function imprimirPalavra(){
+        return(
+            <span className="letra-palavra">_</span>
+        )
     }
 
     const alfabeto = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
@@ -33,7 +49,9 @@ function App() {
             <div className="imagem-botao">
                 <img className="forca" src={forca0} alt="imagem da forca" />
                 <button className="botao-escolher" onClick={habilitarJogo}>Escolher Palavra</button>
+                <div className="palavra-jogo">{palavraEscolhida.map((a)=>(letraEscolhida.includes(a))? <span className="letra-palavra">{a}</span>:<span className="letra-palavra">_</span>)}</div>
             </div>
+
 
             <div className="painel-letras">
                 {alfabeto.map(imprimirLetras)}
